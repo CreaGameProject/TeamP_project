c16b541ed18618bd07d624d51cc04b3072e318fb
Tag-m <52032585+Tag-m@users.noreply.github.com>
2019-11-14 12:37:20 +0900

    ボスのダメージ⇒ゲームクリア関係

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    int Bosshp = 5;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.transform.tag == "Enemy")
+        {
+            Bosshp--;
+
+            //images[Bosshp].SetActive(false);余裕があれば表示させます
+
+            if (Bosshp == 0)
+            {
+                GoToGameClear();
+            }
+            
+            Debug.Log(collision.gameObject.name);
+
+        }
+    }
+    private void GoToGameClear()
+    {
+        SceneManager.LoadScene("GameClearScene");
+    }
+    
+}
