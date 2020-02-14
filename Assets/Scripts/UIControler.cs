cbc2d0531f13a79d4fb4a9a34649a6c1d895b91f
Keijiro-cgp <Keijiro.cgp@gmail.com>
2020-02-14 11:13:24 +0900

    PostProcess周りの問題の解消
    UIControllerクラスの適用されているオブジェクトを各ボタン→MainCameraに変更してます。その他細かい調整(レイヤーを追加など)。

--- Assets/Scripts/UIControler.cs
+++ Assets/Scripts/UIControler.cs
@@ -13,20 +13,33 @@ public class UIControler : MonoBehaviour
     public GameObject TitleImage;
     public GameObject Life;
 
-
     [SerializeField]
-    GameObject postProcessGameObject;
+    PostProcessVolume postProcessVolume;  //PostProcessの適用を管理してるクラス
+    PostProcessProfile postProcessProfile;  //適用する内容が保存されてるクラス
+
+    DepthOfField dof;  //DepthOfFieldの設定の内容が入るクラス
+
+    private Player player_sc;  //Playerクラス継承用
+    private bool is_gameover = false;  //ゲームオーバー時の処理を一回のみ行うための変数
 
     // Start is called before the first frame update
     void Start()
     {
-
+        player_sc = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();  //Playerクラスを取得
+        postProcessProfile = postProcessVolume.profile;  //PostProcessProfileを取得
+        dof = postProcessProfile.GetSetting<DepthOfField>();  //DepthOfFieldを取得
+        dof.focusDistance.Override(0.1f);  //ぼかしをかける
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        //プレイヤーのHPが0になったとき一度だけ画面をぼかす
+        if (player_sc.hp == 0 && !is_gameover)
+        {
+            is_gameover = true;
+            dof.focalLength.Override(0.1f);
+        }
     }
     public void Startclick()
     {
@@ -34,7 +47,7 @@ public class UIControler : MonoBehaviour
         EndButton.SetActive(false);
         TitleImage.SetActive(false);
         Life.SetActive(true);
-        Invoke("FixDOF", 1f);
+        FixDOF();
     }
     public void Retryclick()
     {
@@ -44,9 +57,7 @@ public class UIControler : MonoBehaviour
     //ぼかすのを治す
     void FixDOF()
     {
-        var dof = ScriptableObject.CreateInstance<DepthOfField>();
-        dof.focusDistance.Override(4);
-        PostProcessManager.instance.QuickVolume(postProcessGameObject.layer, 1, dof);
+        dof.focusDistance.Override(10);
     }
     //ゲーム終了
     public void EndGame()
