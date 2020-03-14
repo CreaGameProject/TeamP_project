60407ed80a9f3850eb0958087004862939ccd106
Tag-m <52032585+Tag-m@users.noreply.github.com>
2020-03-14 22:00:09 +0900

    ボスのアニメーション２

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,64 +4,79 @@ using System.Collections;
 
 
 public class Boss : MonoBehaviour
-{
-    [SerializeField]
+{    
+    /*[SerializeField]
     float position1 = 220f;
 
     [SerializeField]
-    float position2 = 184f;
+    float position2 = 184f;*/
+    
+    int AttackType;         //！Animatorで宣言した
+    bool AttackManager;     //！   パラメーターに変えたい
+    Animator animator;
 
-    private int rushMode = 0;  //突進攻撃
-    private int repeat = 0;  //繰り返す回数＝＝繰り返した回数かを判定
-    int attackType = 0;  //攻撃選択
-    bool attackManager = false;
-    private float count, NowTime = 0f; 
+    private int count = 0;//繰り返す回数＝＝繰り返した回数かを判定
+
+    public int Bosshp = 5;
 
     private void Start()
     {
+        animator = GetComponent<Animator>();  
     }
 
     void Update()
     {
-        int attackTimes = Random.Range(0, 3)+1;  //攻撃回数
-        count += Time.deltaTime;
+        animator.SetInteger("AttackType", 0);     //！ここで
+        animator.SetBool("AttackManager", false); //！   いいのか
 
-        if (attackManager == false)  //最初と攻撃終了時に攻撃を選択  
+        int attackTimes = Random.Range(0, 3) + 1;  //攻撃回数
+
+        if (AttackManager == false)  //最初と攻撃終了時に攻撃を選択  
         {
             Debug.Log("攻撃を選択中");
-            attackType = Random.Range(0, 2);
-            attackManager = true;
-            
-            Debug.Log("攻撃タイプ：" + attackType + "　攻撃回数：" + attackTimes );
+            AttackType = Random.Range(0, 2);
+            AttackManager = true;
 
+            Debug.Log("攻撃タイプ：" + AttackType + "　攻撃回数：" + attackTimes);
         }
 
-        else if (attackManager == true)
-        {            
-            if (attackType == 0)  //攻撃回数秒待機(仮)
+        else if (AttackManager == true)
+        {
+            if (AttackType == 0)  //雑魚を投げる攻撃
             {
-                if (count >= 1.0f && NowTime < attackTimes)
+                if (count != attackTimes)
                 {
-                    NowTime += 1 ;
-                    Debug.Log( NowTime + "秒 待機中");
-                    count = 0.0f;
-                    Vector3 pos = this.gameObject.transform.position;
-                    this.gameObject.transform.position = new Vector3(pos.x, pos.y, pos.z);
+                    Debug.Log("投げ攻撃中");
+
+                    count += 1;
                 }
 
-                else if (count >= 1.0f && NowTime >= attackTimes)
+                else if (count == attackTimes)
                 {
-                    Debug.Log("待機終了");
-                    attackManager = false;
+                    Debug.Log("投げ攻撃終了");
+                    AttackManager = false;
+                    count = 0;
                     return;
                 }
             }
 
-            
-            else if (attackType == 1)
-            {                
-                Debug.Log("突進攻撃中");
+            else if (AttackType == 1)
+            {
+                if (count != attackTimes)
+                {
+                    Debug.Log("突進攻撃中");
 
+                    count += 1;
+                }
+
+                else if (count == attackTimes)
+                {
+                    Debug.Log("突進攻撃終了");
+                    AttackManager = false;
+                    count = 0;
+                    return;
+                }
+                {/* 
                 if (rushMode == 0)
                 {
                     //Debug.Log("BossMode0");
@@ -121,23 +136,19 @@ public class Boss : MonoBehaviour
                         repeat += 1 ;
                         Debug.Log(repeat + "回目");
                     }
-                }
+                }*/
+                }     
             }
         }
     }
 
-    public int Bosshp = 5;
-
     public void OnCollisionEnter(Collision other) //ボスのHP処理→ゲームクリアシーン読み込み
     {
-        //Debug.Log(other.transform.name);
-
         //GetConmponent 別のスクリプトを取得
         if (other.transform.GetComponent<Zako1>())
         {
             if (other.transform.GetComponent<Zako1>().is_attacked)
             {
-
                 //雑魚がボスに五回当たったらゲームクリア
                 if (other.transform.tag == "Enemy")
                 {
@@ -151,12 +162,9 @@ public class Boss : MonoBehaviour
 
                         SceneManager.LoadScene("GameClear");
                     }
-
-                    //Debug.Log(other.transform.name);
                     Debug.Log("Boss's HP is " +  Bosshp);
-
                 }
-             }
+            }
         }
     }
     /*public void GoToGameClear()
